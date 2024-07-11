5589624811c6d7f7eea12f1ef03abcaf26fa93e3
sidantha0377 <150885218+sidantha0377@users.noreply.github.com>
2024-07-11 17:37:32 +0530

    Create food.dart

---
+++ lib/models/food.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Food {
+  final String name;
+  final String category;
+  final String price;
+  final String disprice;
+  final String description;
+  final double amount;
+  final String imgeUrl;
+  bool isFavourit;
+
+  Food{
+    required this.name,
+    required this.category,
+    required this.price,
+    required this.disprice,
+    required this.description,
+    
+
+  };
+}
